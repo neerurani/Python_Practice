36b50c218e966013b302f68ebd20b328dee7d303
Neeru Rani <ranineeru1992@gmail.com>
2020-01-13 18:32:25 +0530

    Meyhods for pattern

--- pattern.py
+++ pattern.py
@@ -9,7 +9,7 @@ Created on 10-06-2019
 * * * *
 pattern '''
 
-def patternPiramid(noOfRow,char):
+def piramidPattern(noOfRow,char):
     n=int(noOfRow)
     for i in range(0,n):
         for j in range(0,n-i-1):
@@ -19,4 +19,36 @@ def patternPiramid(noOfRow,char):
         print() # for next line
 
 nor=input("Enter the no. of rows: ")
-patternPiramid(nor,"*")
+#piramidPattern(nor,"*")
+
+''' Program to print 
+*
+**
+***
+****
+*****
+   pattern  '''
+def ladderPattern(noOfRow,char):
+    n=int(noOfRow)
+    for i in range(0,n+1):
+        for j in range(0,i+1):
+            print(char,end=" ")
+        print()
+
+#ladderPattern(nor,"#")
+
+'''  Program to print
+1
+12
+123
+1234
+12345
+   pattern  '''
+def digitPattern1(noOfRow):
+    n=int(noOfRow)
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(j,end=" ")
+        print()
+
+digitPattern1(5)
