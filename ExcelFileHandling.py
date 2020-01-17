286bf8889938bfa9a119fdabe862d0308a4aa72c
Neeru Rani <ranineeru1992@gmail.com>
2020-01-17 16:34:54 +0530

    Methods for file handling

--- ExcelFileHandling.py
+++ ExcelFileHandling.py
@@ -4,9 +4,10 @@ Created on 22-06-2019
 '''
 
 import openpyxl
+import pandas as pd
 
 #................... Method to Open an excel file....................
-def penExcel(filePath):
+def openExcel(filePath):
     wb_obj = openpyxl.load_workbook(filePath)
     sheet_obj = wb_obj.active
     return sheet_obj,wb_obj
@@ -42,6 +43,47 @@ for i in range(0,nor):
     insertInExcel(path)
 readExcel(path)'''
 
+#................... Method to update data in excel file....................
+def updateExcel(existVal,newVal,fpath):
+    st_obj, w_obj = openExcel(fpath)
+    for i in range(1, st_obj.max_row+1):
+        for j in range(1, st_obj.max_column + 1):
+            cell_obj = st_obj.cell(row=i, column=j)
+            if cell_obj.value == existVal:
+                cell_obj.value = newVal
+    w_obj.save(fpath)
+
+'''path = "C:\\neeru\\Python_Practice\\ItemDetails.xlsx"
+updateExcel("aa","hd",path)
+readExcel(path)'''
+
 #................... Method to delete data from excel file....................
 def deleteFromExcel():
-    
+    # Instantiating a Workbook object by excel file path
+    wb_obj = self.Workbook(self.dataDir + 'Book1.xls')
+    # Accessing the first worksheet in the Excel file
+    worksheet = workbook.getWorksheets().get(0)
+
+    # Deleting 3rd row from the worksheet
+    worksheet.getCells().deleteRows(2, 1, True)
+
+    # Saving the modified Excel file in default (that is Excel 2003) format
+    workbook.save(self.dataDir + "Delete Row.xls")
+
+    print
+    "Delete Row Successfully."
+
+
+
+'''st_obj.move_range("D4:B7", rows=-1, cols=2)
+wb_obj.save(path)'''
+
+
+'''data = pd.read_csv("nba.csv", index_col="Name")
+
+# dropping passed values 
+data.drop(["Avery Bradley", "John Holland", "R.J. Hunter",
+           "R.J. Hunter"], inplace=True)
+
+# display 
+data '''
